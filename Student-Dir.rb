69c04ef4185393267febe2e70b935f83425f4f44
Berta <berta.gutierrez@gmail.com>
2014-01-09 18:44:27 +0000

    Menu added

--- Student-Dir.rb
+++ Student-Dir.rb
@@ -1,33 +1,23 @@
 #!/usr/bin/env ruby
 require 'date'
 
-students = [{name: "Berta", cohort: :Jan, birthdate: "1980-01-01", country: :UK},
-			{name: "Paul", cohort: :Jan, birthdate: "1980-01-01", country: :France},
+@students = [{name: "Berta", cohort: :Jan, birthdate: "1980-01-01", country: :UK},
+			{name: "Paul", cohort: :Jan, birthdate: "1981-01-01", country: :France},
 			{name: "Megan", cohort: :Jan, birthdate: "1980-01-01", country: :Spain},
 			{name: "Rolando", cohort: :Jan, birthdate: "1980-01-01", country: :UK},
 			{name: "James", cohort: :Nov, birthdate: "1990-10-07", country: :UK}]
-#students = []
-def list(students)
 
 
-#	users.each_with_index do |student, i|   print "#{i+1} - #{student[:name]} (#{student[:cohort]} cohort)\n" if student[:name].length < 12
-#	end
 
-#Change each for challenge 4
-	x = 20 # Width of each 'column'
-	i=0
-	puts "\n      " + "NAME".ljust(x) + "COHORT".ljust(x) + "DATE OF BIRTH".ljust(x) + "COUNTRY OF BIRTH".ljust(x)
-	puts "="*80  
-
-	while i < students.length do
-		num= i+1
-		puts "#{num.to_s.rjust(3)} - #{students[i][:name].to_s.ljust(x)} #{students[i][:cohort].to_s.ljust(x)} #{students[i][:birthdate].to_s.ljust(x)} #{students[i][:country].to_s.ljust(x)}"
-	    i+=1
+def list_students(students_sorted)
+	x = 20 #width of the columms
+	students_sorted.each_with_index do |student, i| puts "#{i+1} - #{student[:name].to_s.ljust(x)} #{student[:cohort].to_s.ljust(x)} #{student[:birthdate].to_s.ljust(x)} #{student[:country].to_s.ljust(x)}"
 	end
 end
 
-def order_by(students, field=:name)
-	students_sorted = students.sort{|x,y| x[field].to_s <=> y[field].to_s}
+def order_by(field)
+	#students_sorted = @students.sort{|x,y| x[field].to_s <=> y[field].to_s}
+	@students.sort_by {|hsh| hsh[field]}
 end
 
 def is_name(name)
@@ -41,73 +31,143 @@ def is_cohort(cohort)
 end
 
 def is_date(date)
-
-	
 	Date.valid_date?(date.slice(6,4).to_i,date.slice(3,2).to_i,date.slice(0,2).to_i)
-
 end
 
-def insert_student(students)
-
+def get_name()
 	print "Enter name: "
 	name = gets.chomp.capitalize()
 	while (!is_name(name))
-			puts "Not allowed, Try again."
+			prints "Name not allowed, Try again."
 			name = gets.chomp.capitalize()
 	end
+	name
+end
 
+def get_cohort()
 	print "Enter cohort: "
 	cohort = gets.chomp.capitalize()
 	#cohort = default if cohort.empty?
-	while (!(is_cohort(cohort) || cohort.empty?))
+	while (!is_cohort(cohort) && !cohort.empty?)
 		puts "again"
 		cohort = gets.chomp.capitalize()
 	end
-	cohort = cohort.empty? ? nil : cohort.to_sym
+	cohort
+end
 
-	print "Enter date of birth: "
+def get_birthdate()
+	print "Date of birth (DD/MM/YYYY): "
 	birthdate = gets.chomp()
-	
-	while (!(is_date(birthdate) || birthdate.empty?))
-		puts "again"
-		birthdate = gets.chomp.capitalize()
+	while (!is_date(birthdate) && !birthdate.empty?) 
+		puts "Wrong date, try again: "
+		birthdate = gets.chomp()
 	end
+	birthdate
+end
 
-	birthdate = nil if birthdate.empty? 
-
-
+def get_country()
 	print "Enter Country of birth: "
 	country = gets.delete("\n").capitalize()
-	country = country.empty? ? nil : country.to_sym
-
-	students << {:name => name, :cohort => cohort, :birthdate => birthdate, :country => country}
-		
-	
 end
+
+def new_student()
+
+	loop do 
+		name = get_name
+		cohort = get_cohort
+		birthdate = get_birthdate
+		country = get_country
+
+		cohort = cohort.empty? ? nil : cohort.to_sym
+		birthdate = nil if birthdate.empty? 
+		country = country.empty? ? nil : country.to_sym
+
+		@students << {:name => name, :cohort => cohort, :birthdate => birthdate, :country => country}		
+		print "Insert more students? (Y/N) >> "
+
+		return if gets.chomp.capitalize() == "N"
+	end
+end
+
 
 def print_header
 	puts "\nCLASS DIRECTORY"
 	puts "==============="
 end
 
-def print_footer(n)
-	es = (n != 1) ? "s" : ""
-	puts "Now we have #{n} student#{es}"
+def print_footer
+	es = (@students.length != 1) ? "s" : ""
+	puts "Now we have #{@students.length} student#{es}"
 	puts "-"*80
 	puts " " 
 
 end
 
-students = []
-
-if !students.empty? 
-	print_header
-	list(order_by(students, :cohort))
-	print_footer(students.length) 
+def show_students
+	if !@students.empty? 
+		print_header
+		list_students(order_by(:name))
+		print_footer
+		sub_menu1
+	else
+		puts "There are no students to list.\n"
+	end
 end
 
-insert_student(students)
+def sub_menu1
+
+	loop do  
+	 puts "Sort options: <1> by cohort, <2> by date of birth, <3> by country of birth <9> to go back to main manu."
+	 print ">> "
+	 case option = gets.chomp 
+	 when "1"
+	 	list_students(order_by(:cohort))
+	 when "2"
+	 	list_students(order_by(:birthdate))
+	 when "3"
+	 	list_students(order_by(:country))
+	 when "9"
+	 	return
+	 else
+	 	puts "Option not valid, try again"
+	 end
+	end
+
+end
+
+
+def main_menu()
+	loop do
+		puts ""
+		puts "STUDENTS DIRECTORY".center(100)
+		puts "=======================================\n".center(100)
+
+		puts "1 - List the studens."
+		puts "2 - Insert new students."
+		puts "9 - Exit\n\n"
+
+		print "Enter option: "
+
+		selection = gets.chomp
+
+		case selection
+		when "1"
+			show_students
+		when "2"
+			new_student
+		when "9"
+			exit
+		else
+			print "Option not valid, try again: "
+		end
+	end
+end	
+
+
+main_menu
+
+#new_student(students)
 
 #puts Date::ABBR_MONTHNAMES
-list(order_by(students, :cohort))
-print_footer(students.length)
+#list(order_by(:cohort))
+#print_footer(students.length)
